88383778125a578d1c28b3ca9fbbf8e7d79aada7
[AloneAsif] <[asifleon3@gmail.com]>
2024-03-11 23:09:22 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,24 @@
+// Question no 30   Hello Admin
+
+let userNames: string[] = ['asif','yasir','admin','yaseen','atif'];
+
+for(let userName of userNames){
+    if(userName == 'admin'){
+        console.log('Hello admin, would you like to see a status report?')
+    }
+    else if(userName != 'admin'){
+        console.log(`hello ${userName} welcome!`)   
+    }
+    // else if(userName == 'yaseen'){
+    //     console.log(`hello ${userName} welcome!`)   
+    // }
+    // else if(userName == 'atif'){
+    //     console.log(`hello ${userName} welcome!`)   
+    // }
+    // else if(userName == 'yasir'){
+    //     console.log(`hello ${userName} welcome!`)   
+    // }
+    // else if(userName == 'asif'){
+    //     console.log(`hello ${userName} welcome!`)   
+    // }
+}
